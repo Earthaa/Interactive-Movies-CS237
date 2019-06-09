5d840068aab6f9f473efc08611281c7653c55de7
Earthaa <2892212@qq.com>
2019-06-08 20:21:54 -0700

    update

---
+++ src/main/java/com/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.constants;
+
+public class Constants {
+public static String NamenodeIP="127.0.0.1";//
+public static String NamenodePort="8020";
+public static String HDFSAddress="hdfs://"+NamenodeIP+":"+NamenodePort;
+}
